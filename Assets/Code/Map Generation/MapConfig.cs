cdd05aeff241e203ca5073bd488be09869b966a0
Aric Quan <aric.quan@liftwerx.com>
2026-04-23 19:12:26 -0400

    fixed map gen dist

--- Assets/Code/Map Generation/MapConfig.cs
+++ Assets/Code/Map Generation/MapConfig.cs
@@ -25,7 +25,7 @@ public class MapConfig : ScriptableObject
 
     [Header("room spacing")]
     [SerializeField] public float standardRoomWidth = 16f; // z spacing between adjacent standard rooms
-    [SerializeField] public float spawnToFirstLayer = 15.19f; // x spacing from spawn room to layer 1
+    [SerializeField] public float spawnToFirstLayer = 16f; // x spacing from spawn room to layer 1
     [SerializeField] public float layerToControlRoom = 4.1f; // x spacing from last standard layer to control room
     [SerializeField] public float standardLayerStep = 16f; // x spacing between middle standard layers
 }
